265efe335450786ef21710def77061fa4e0cddfe
EMCodeStudio <estivenmc53@gmail.com>
2023-03-28 22:07:12 -0500

    update product

--- mmstore/src/Components/Banner/Data_Banner.jsx
+++ mmstore/src/Components/Banner/Data_Banner.jsx
@@ -4,30 +4,35 @@ export const dataBanner = [
         id:1,
         title:"Accesorios",
         subtitle:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, autem minima.",
-        img:"https://images.pexels.com/photos/135620/pexels-photo-135620.jpeg?auto=compress&cs=tinysrgb&w=1920&h=1080&dpr=1"
+        img:"https://images.pexels.com/photos/135620/pexels-photo-135620.jpeg?auto=compress&cs=tinysrgb&w=1920&h=1080&dpr=1",
+       
     },
     {
         id:2,
         title:"Tecnologia",
         subtitle:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, autem minima.",
-        img:"https://images.pexels.com/photos/230544/pexels-photo-230544.jpeg?auto=compress&cs=tinysrgb&w=1920&h=1080&dpr=1"
+        img:"https://images.pexels.com/photos/230544/pexels-photo-230544.jpeg?auto=compress&cs=tinysrgb&w=1920&h=1080&dpr=1",
+       
     },
     {
         id:3,
         title:"Moda",
         subtitle:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, autem minima.",
-        img:"https://images.pexels.com/photos/974911/pexels-photo-974911.jpeg?auto=compress&cs=tinysrgb&w=1920&h=1080&dpr=1"
+        img:"https://images.pexels.com/photos/974911/pexels-photo-974911.jpeg?auto=compress&cs=tinysrgb&w=1920&h=1080&dpr=1",
+       
     },
     {
         id:4,
         title:"Calzado",
         subtitle:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, autem minima.",
-        img:"https://images.pexels.com/photos/298863/pexels-photo-298863.jpeg?auto=compress&cs=tinysrgb&w=1920&h=1080&dpr=1"
+        img:"https://images.pexels.com/photos/298863/pexels-photo-298863.jpeg?auto=compress&cs=tinysrgb&w=1920&h=1080&dpr=1",
+       
     },
     {
         id:5,
         title:"Cuidado Personal",
         subtitle:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, autem minima.",
-        img:"https://images.pexels.com/photos/4202329/pexels-photo-4202329.jpeg?auto=compress&cs=tinysrgb&w=1920&h=1080&dpr=1"
+        img:"https://images.pexels.com/photos/4202329/pexels-photo-4202329.jpeg?auto=compress&cs=tinysrgb&w=1920&h=1080&dpr=1",
+       
     },
 ]
--- mmstore/src/Components/Products/Data_Products.jsx
+++ mmstore/src/Components/Products/Data_Products.jsx
@@ -6,7 +6,9 @@ export const dataProducts = [
         image:"https://images.pexels.com/photos/267301/pexels-photo-267301.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
         title:"Zapato",
         description:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel magni quisquam, asperiores eius earum illum odio, delectus voluptas in impedit numquam unde quibusdam nemo eligendi nesciunt tempore perspiciatis dolore rerum?",
-        price:"645.000"
+        price:"645.000",
+        offer:false,
+        discount:0
     },
     
     {
@@ -14,7 +16,9 @@ export const dataProducts = [
         image:"https://images.pexels.com/photos/818043/pexels-photo-818043.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
         title:"Smartphone",
         description:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel magni quisquam, asperiores eius earum illum odio, delectus voluptas in impedit numquam unde quibusdam nemo eligendi nesciunt tempore perspiciatis dolore rerum?",
-        price:"4.400.000"
+        price:"4.400.000",
+        offer:false,
+        discount:0
     },
     
     {
@@ -22,14 +26,18 @@ export const dataProducts = [
         image:"https://images.pexels.com/photos/39716/pexels-photo-39716.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
         title:"Lentes",
         description:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel magni quisquam, asperiores eius earum illum odio, delectus voluptas in impedit numquam unde quibusdam nemo eligendi nesciunt tempore perspiciatis dolore rerum?",
-        price:"480.000"
+        price:"480.000",
+         offer:false,
+        discount:0
     },
     
     {
         id:4,
         image:"https://images.pexels.com/photos/3910071/pexels-photo-3910071.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        title:"Perfume",
+        title:"Camisa",
         description:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel magni quisquam, asperiores eius earum illum odio, delectus voluptas in impedit numquam unde quibusdam nemo eligendi nesciunt tempore perspiciatis dolore rerum?",
-        price:"380.000"
+        price:"380.000",
+        offer:false,
+        discount:0
     }
 ]
