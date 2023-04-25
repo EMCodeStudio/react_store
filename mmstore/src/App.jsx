ba65553400e839935a6bdb1f236381415d1a35e6
EMCodeStudio <estivenmc53@gmail.com>
2023-04-24 23:21:09 -0500

    update axios

--- mmstore/src/App.jsx
+++ mmstore/src/App.jsx
@@ -1,21 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.scss'
 import Home from './Routes/Home/Home'
-
 function App() {
-
-
   return (
-
-    <>
     <div className="app">
       <Home />
-
     </div>
-    </>
-
+    
   )
 }
 
--- mmstore/src/Components/Products/Products.jsx
+++ mmstore/src/Components/Products/Products.jsx
@@ -1,11 +1,11 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Card_Product from '../Card_Product/Card_Product'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
-import { dataProducts } from './Data_Products'
+/* import { dataProducts } from './Data_Products' */
 import './Products.scss'
 import { TitleProduct } from '../Styled/Titles/Titles'
-import { servicesProducts } from '../../Services/Products/Services_Products'
+import { servicesProducts } from '../../Services/Products_Services/Services_Products'
 
 const Products = () => {
 
